0b6fdd65897adbba424355ec905e380c811e95a1
widowild <widomaker2k7@gmail.com>
2011-01-22 21:48:08 +0100

    ajout /bin/env/python

--- python/label.py
+++ python/label.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf8 -*-
+#=================
+
 from tkinter import *
 root=Tk()
- 
+
 def changeLabel():
     myString.set("I'm, a-fraid we're fresh out of red Leicester, sir. ")
- 
+
 myString=StringVar()
 Label(root,textvariable=myString).pack()
 myString.set("Well, eh, how about a little red Leicester.")
